30262b5565491b90ceb0d2b96eeed68e09840596
Brian Floersch <gh123man@gmail.com>
2023-06-26 21:43:07 -0400

    cleanup

--- Sources/ScrollViewLoader/ScrollViewLoader.swift
+++ Sources/ScrollViewLoader/ScrollViewLoader.swift
@@ -78,40 +78,4 @@ class ScrollOffsetNotifier: NSObject, UIScrollViewDelegate, ObservableObject {
             canNotify = bottomOffset >= triggerHeight || oldContentHeight != scrollView.contentSize.height
         }
     }
-    
-    func computeState() {
-        
-    }
-}
-
-struct ContentView: View {
-    
-    @State var data: [Int] = Array(0..<1)
-    
-    var body: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach(data, id: \.self) { i in
-                    Text("\(i)")
-                        .font(.title)
-                        .frame(maxWidth: .infinity)
-                }
-                ProgressView()
-                    .scaleEffect(2)
-            }
-        }
-        .shouldLoadMore {
-            await Task.sleep(seconds: 0.1)
-//            data.append(contentsOf: (data.last! + 1)...data.last! + 100)
-            data.append(data.last! + 1)
-        }
-    }
-}
-
-
-extension Task where Success == Never, Failure == Never {
-    static func sleep(seconds: Double) async {
-        let duration = UInt64(seconds * 1_000_000_000)
-        try! await Task.sleep(nanoseconds: duration)
-    }
 }
